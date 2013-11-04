881454eafa66a7579e9a688e200090c4b84e5a66
podgorm <mike04016@gmail.com>
2013-11-04 17:21:12 -0500

    Added helper methods

--- Car.java
+++ Car.java
@@ -1,13 +1,32 @@
-
 public class Car {
 
+	public static final int CAPACITY = 4;
+
 	public String destination;
-	public Rider[] riders;
+	private Rider[] riders;
+	private int numRiders;
 	public boolean isFull;
 	
 	public Car(String destination) {
 		this.destination = destination;
-		riders = new Rider[4];
+		riders = new Rider[CAPACITY];
+		numRiders = 0;
 		isFull = false;
+	}
+	
+	public void addRider(Rider r) {
+		if (isFull)
+			return;
+		
+		riders[numRiders++] = r;
+		isFull = (numRiders == CAPACITY);
+	}
+	
+	public boolean isFull() {
+		return isFull;
+	}
+	
+	public String getDestination() {
+		return destination;
 	}
 }
