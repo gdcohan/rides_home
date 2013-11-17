9c76664605ae5b62fc3807323719bf12d3ff469c
podgorm <mike04016@gmail.com>
2013-11-17 18:28:06 -0500

    Added cost calculation

--- Car.java
+++ Car.java
@@ -2,6 +2,8 @@ public class Car {
 
     public static final int CAPACITY = 4;
     
+    private static HashMap<String, HashMap<String, Double>> distances;
+    
     public String destination;
     private Rider[] riders;
     private int numRiders;
@@ -15,6 +17,10 @@ public class Car {
 	isFull = false;
     }
     
+    public static void setDistanceMatrix(HashMap<String, HashMap<String, Double>> distanceMatrix) {
+    	distances = distanceMatrix;
+    }
+    
     public void addRider(Rider r) {
 	if (isFull || r.isAssigned())
 	    return;
@@ -22,6 +28,70 @@ public class Car {
 	riders[numRiders++] = r;
 	r.setAssigned(true);
 	isFull = (numRiders == CAPACITY);
+    }
+    
+    public void setRiders(Rider[] toSet) {
+    	riders = toSet.clone();
+    	numRiders = toSet.length;
+    }
+    
+    public double getCost() {
+    	
+    	//1: Initialize a matrix of distances
+    	double[][] carCosts = new double[CAPACITY+2][CAPACITY+2];
+    	//carCosts[i][j] is dist from i to j 
+    	
+    	//Amherst to others
+    	for (int i=1; i<carCosts.length-1; i++) {
+    		carCosts[0][i] = distances.get("Amherst, MA").get(riders[i-1].getDestination());
+    	}
+    	//carCosts[0][carCost[0].length-1] = distances.get("Amherst, MA").get(destination);
+    	
+    	//Intra-riders
+    	for (int i=1; i<carCosts.length-1; i++) {
+    		for (int j=1; j<carCosts[i].length-1; j++) {
+    			carCosts[i][j] = distances.get(riders[i-1].getDestination()).get(riders[j-1].getDestination());
+    		}
+    	}
+    	
+    	//Riders to driver
+    	for (int i=1; i<carCosts.length-1; i++) {
+    		carCosts[carCosts.length-1][i] = distances.get(riders[i-1].getDestination()).get(destination);
+    	}
+    	
+    	//2: Greedily construct route
+    	LinkedList<Integer> order = new LinkedList<Integer>();
+    	order.add(0);
+    	for (int i=1; i<carCosts.length-1; i++) {
+    		order.addLast(findNextStop(carCosts[order.getLast()], order));
+    	}
+    	order.add(carCosts[0].length-1);
+    	
+    	//3: Return the total cost
+    	double totalCost = 0;
+    	for (int i=1; i<order.size(); i++) {
+    		totalCost += carCosts[order.get(i-1)][order.get(i)];
+    	}
+    	
+    	return totalCost;
+    		
+    	
+    }
+    
+    private int findNextStop(double[] dists, LinkedList<Integer> excluding) {
+    	//Don't check first or last stop
+    	double minCost = Double.MAX_VALUE;
+    	int minIndex = -1;
+    	for (int i=1; i<dists.length-1; i++) {
+    		if(!excluding.contains(i)) {
+    			double cost = dists[i];
+    			if (cost < minCost){
+    				minCost = cost;
+    				minIndex = i;
+    			}
+    		}
+    		
+    	}
     }
     
     public void swapRiders(Rider departing, Rider arriving) {
